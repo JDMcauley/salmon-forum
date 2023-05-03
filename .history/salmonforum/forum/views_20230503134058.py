1179cb99bff3482b0cb2246ced59ece81d30cb2c
JDMcauley <123961794+JDMcauley@users.noreply.github.com>
2023-05-03 15:38:08 +0100

    added-templates
    Added superuser to log into /admin, added base.html for future templates, and added home.html for homepage.

---
+++ .history/salmonforum/forum/views_20230503134058.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(response, "forum/base.html", {})
+
+def home(response):
+    return render(response, "forum/home.html")
---
+++ .history/salmonforum/forum/views_20230503141946.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(response):
+    return render(response, "forum/home.html")
+
+def home(response):
+    return render(response, "forum/home.html")
